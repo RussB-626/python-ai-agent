f2828a8c8ce692a806c68a50ff9c103b786ed800
RussB-626 <russblaine.626@gmail.com>
2025-07-29 21:09:16 +0000

    refactored get_files_info and added get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,10 +2,17 @@ import os
 
 def get_files_info(working_directory, directory="."):
   try:
-    target_path = get_target_path(working_directory, directory)
+    working_abs = os.path.abspath(working_directory)
+    target_abs = os.path.abspath(os.path.join(working_directory, directory))
+    
+    if not target_abs.startswith(working_abs):
+      raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if not os.path.isdir(target_abs):
+      raise Exception(f'Error: "{directory}" is not a directory')
+
     file_results = []
-    for filename in os.listdir(target_path):
-      file_path = os.path.join(target_path, filename)
+    for filename in os.listdir(target_abs):
+      file_path = os.path.join(target_abs, filename)
       is_dir = os.path.isdir(file_path)
       file_size = os.path.getsize(file_path)
 
@@ -15,17 +22,3 @@ def get_files_info(working_directory, directory="."):
 
   except Exception as e:
     return f"Error listing files: {e}"
-  
-def get_target_path(working_directory, directory):
-  # Build the full path and normalize both paths
-  working_abs = os.path.abspath(working_directory)
-  target_abs = os.path.abspath(os.path.join(working_directory, directory))
-  
-  # Determin if the target directory is outside the working directory, 
-  # and if the target directory is a directory
-  if not target_abs.startswith(working_abs):
-    raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-  if not os.path.isdir(target_abs):
-    raise Exception(f'Error: "{directory}" is not a directory')
-  
-  return target_abs
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def test():
   result = get_files_info("calculator", ".")
@@ -16,6 +17,26 @@ def test():
 
   result = get_files_info("calculator", "../")
   print("Result for '../' directory:")
+  print(result)
+
+  result = get_file_content("calculator", "main.py")
+  print("Result for 'main.py' file:")
+  print(result)
+
+  result = get_file_content("calculator", "pkg/calculator.py")
+  print("Result for 'pkg/calculator.py' file:")
+  print(result)
+
+  result = get_file_content("calculator", "/bin/cat")
+  print("Result for '/bin/cat' file:")
+  print(result)
+
+  result = get_file_content("calculator", "pkg/does_not_exist.py")
+  print("Result for 'pkg/does_not_exist.py' file:")
+  print(result)
+
+  result = get_file_content("calculator", "lorem.txt")
+  print("Result for 'lorem.txt' file:")
   print(result)
 
 
