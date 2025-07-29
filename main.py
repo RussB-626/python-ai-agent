09fa922cfae94b10bb43cd9c27013f2baef0b80a
RussB-626 <russblaine.626@gmail.com>
2025-07-29 03:46:46 +0000

    added command line prompting

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
 
 def main():
-  load_dotenv()
-  api_key = os.environ.get("GEMINI_API_KEY")
-  client = genai.Client(api_key=api_key)
+  try:
+    if len(sys.argv) == 1:
+       print("Usage: uv run main.py <prompt>")
+       sys.exit(1)
 
-  response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-  )
-  print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-  print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
-  print("Response:")
-  print(response.text)
+    user_prompt = sys.argv[1]
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+      model='gemini-2.0-flash-001',
+      contents=user_prompt
+    )
+    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    print("Response:")
+    print(response.text)
+
+  except Exception as e:
+     print(f"Error Encountered: {e}")
 
 
 if __name__ == "__main__":
