44f3f04601a47dd23cc17001f913a0e7279a70ac
RussB-626 <russblaine.626@gmail.com>
2025-07-29 03:38:20 +0000

    fixed variable declarations

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
-from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+from dotenv import load_dotenv
 
 def main():
+  load_dotenv()
+  api_key = os.environ.get("GEMINI_API_KEY")
+  client = genai.Client(api_key=api_key)
+
   response = client.models.generate_content(
     model='gemini-2.0-flash-001',
     contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
   )
-  print(response.text)
   print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
   print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+  print("Response:")
+  print(response.text)
 
 
 if __name__ == "__main__":
