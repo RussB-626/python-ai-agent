bcfb45d3dd46aad462cf7c26c55923f446ba0a2d
RussB-626 <russblaine.626@gmail.com>
2025-07-31 20:50:52 +0000

    refactored code to match lesson's code

--- call_function.py
+++ call_function.py
@@ -18,40 +18,40 @@ available_functions = types.Tool(
 
 def call_function(function_call_part, verbose=False):
   if verbose:
-    print(print(f"Calling function: {function_call_part.name}({function_call_part.args})"))
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
   else:
     print(f" - Calling function: {function_call_part.name}")
 
-  function_name = function_call_part.name
-  function_args = function_call_part.args
-  function_args["working_directory"] = WORKING_DIRECTORY
+  function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+  }
 
-  match function_name:
-    case "get_files_info":
-      function_result = get_files_info(**function_args)
-    case "get_file_content":
-      function_result = get_file_content(**function_args)
-    case "write_file":
-      function_result = write_file(**function_args)
-    case "run_python_file":
-      function_result = run_python_file(**function_args)
-    case _:
-      return types.Content(
-        role="tool",
-        parts=[
-          types.Part.from_function_response(
-            name=function_name,
-            response={"error": f"Unknown function: {function_name}"},
-          )
-        ],
-      )
+  function_name = function_call_part.name
+
+  if function_name not in function_map:
+    return types.Content(
+      role="tool",
+      parts=[
+        types.Part.from_function_response(
+          name=function_name,
+          response={"error": f"Unknown function: {function_name}"},
+        )
+      ],
+    )
+  
+  args = dict(function_call_part.args)
+  args["working_directory"] = WORKING_DIRECTORY
+  function_result = function_map[function_name](**args)
 
   return types.Content(
-            role="tool",
-            parts=[
-              types.Part.from_function_response(
-                name=function_name,
-                response={"result": function_result},
-              )
-            ],
-          )
+    role="tool",
+    parts=[
+      types.Part.from_function_response(
+        name=function_name,
+        response={"result": function_result},
+      )
+    ],
+  )
--- main.py
+++ main.py
@@ -51,15 +51,23 @@ def generate_content(client, messages, verbose):
       print("Prompt tokens:", response.usage_metadata.prompt_token_count)
       print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-  if response.function_calls:
-     for function_call_part in response.function_calls:
-        call_function_results = call_function(function_call_part, verbose)
-        if not call_function_results.parts[0].function_response.response:
-           raise Exception(f"Error Calling function: {function_call_part.name}({function_call_part.args})")
-        elif verbose:
-           print(f"-> {call_function_results.parts[0].function_response.response}")
-  else:
-     print(response.text)
+  if not response.function_calls:
+     return response.text
+
+  function_responses = []
+  for function_call_part in response.function_calls:
+     function_call_result = call_function(function_call_part, verbose)
+     if(
+        not function_call_result.parts
+        or not function_call_result.parts[0].function_response
+     ):
+        raise Exception("empty function call result")
+     if verbose:
+        print(f"-> {function_call_result.parts[0].function_response.response}")
+  function_responses.append(function_call_result.parts[0])
+
+  if not function_responses:
+     raise Exception("no function responses generated, exiting.")
 
 if __name__ == "__main__":
     main()
